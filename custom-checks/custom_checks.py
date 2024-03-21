ffe98620e8369997605e021d84a8a36f88fabcdf
BickyShaw <bicky.shaw@capgemini.com>
2024-03-21 18:21:36 +0530

    custom checks added

--- custom-checks/custom_checks.py
+++ custom-checks/custom_checks.py
@@ -1,35 +1,23 @@
-from checkov.common.models.enums import CheckCategories, CheckResult
-from checkov.common.util.type_forcers import force_int, force_list
-from checkov.common.multi_signature import MultiSignature
-from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
+from checkov.common.models.enums import CheckCategories
 from checkov.common.models.consts import ANY_VALUE
+from checkov.terraform.checks.resource.base_resource_value_check import BaseResourceValueCheck
 
-class StorageAccountRule(BaseResourceCheck):
+class StorageAccountSKURule(BaseResourceValueCheck):
     def __init__(self):
-        name = "Ensure storage account has encryption enabled"
-        id = "CUSTOM_AZURE_STORAGE_ACCOUNT_ENCRYPTION_ENABLED"
-        supported_resources = ["azurerm_virtual_machine"]
-        categories = [CheckCategories.SECURITY]
+        name = "Ensure storage account SKU is not Premium_LRS"
+        id = "CUSTOM_AZURE_STORAGE_ACCOUNT_SKU"
+        supported_resources = ["azurerm_storage_account"]
+        categories = [CheckCategories.CONVENTION]
 
         super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
 
-        self.multi_signature = MultiSignature(
-            attribute_name="encryption",
-            attribute_type=dict
-        )
+    def get_inspected_key(self):
+        return "sku"
 
-    def scan_resource_conf(self, conf):
-        encryption_settings = conf.get("encryption", None)
+    def get_expected_values(self):
+        return ["Standard_LRS", "Standard_GRS", "Standard_RAGRS", "Standard_ZRS", "Premium_ZRS", "Standard_GZRS", "Standard_RAGZRS"]
 
-        if not encryption_settings:
-            return CheckResult.FAILED
-        
-        if not encryption_settings.get("services"):
-            return CheckResult.FAILED
+    def get_comparison_value(self):
+        return ANY_VALUE
 
-        if "blob" not in force_list(encryption_settings["services"]):
-            return CheckResult.FAILED
-
-        return CheckResult.PASSED
-
-check = StorageAccountRule()
+check = StorageAccountSKURule()
