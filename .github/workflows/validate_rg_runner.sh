e8f9d3286385f7dc929393d664853294be057e78
Bicky Shaw <bicky.shaw988@outlook.com>
2024-04-25 18:18:44 +0530

    passing env variable to bicep command

--- .github/workflows/validate_rg_runner.sh
+++ .github/workflows/validate_rg_runner.sh
@@ -2,20 +2,11 @@ templateFile="./bicep/rg.bicep"
 uuid="$(cat /proc/sys/kernel/random/uuid)"
 location="centralus"
 
-environment=$1
-if [ "$environment" == "dev" ]; then
-    parameterFile="./bicep/rg_parameters_dev.json"
-elif [ "$environment" == "test" ]; then
-    parameterFile="./bicep/rg_parameters_test.json"
-else
-    echo "Invalid environment specified"
-    exit 1
-fi
+
+parameterFile="./bicep/rg_parameters_dev.json"
 
 
-az deployment sub validate --name $uuid --location $location --template-file $templateFile \
-    --parameters $parameterFile 
-    
 
-az deployment sub what-if --name $uuid --location $location --template-file $templateFile \
-    --parameters $parameterFile 
+az deployment sub validate --name $uuid --location $location --template-file $templateFile --parameters $parameterFile
+az deployment sub what-if --name $uuid  --location $location --template-file $templateFile --parameters $parameterFile
+
