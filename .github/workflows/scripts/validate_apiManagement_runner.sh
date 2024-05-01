0b7d8992d093aec39212f65c6b2d67fcb0955c96
Bicky Shaw <bicky.shaw988@outlook.com>
2024-05-02 01:19:42 +0530

    deploying apim

--- .github/workflows/scripts/validate_apiManagement_runner.sh
+++ .github/workflows/scripts/validate_apiManagement_runner.sh
@@ -1,11 +1,11 @@
-templateFile="../bicep/main.bicep"
+templateFile="./bicep/main.bicep"
 uuid="$(cat /proc/sys/kernel/random/uuid)"
 location="centralus"
 resource_group="e37d-sharedservice-rg-001"
 
-parameterFile="../bicep/main_parameters.json"
+parameterFile="./bicep/main_parameters.json"
 
-az deployment group validate --name $uuid --resource-group $resource_group --template-file $template_file --parameters $parameter_file
+az deployment group validate --name $uuid --resource-group $resource_group --template-file $template_file --parameters $parameterFile
     
-az deployment group what-if --name $uuid --resource-group $resource_group --template-file $template_file --parameters $parameter_file \
+az deployment group what-if --name $uuid --resource-group $resource_group --template-file $template_file --parameters $parameterFile \
     
